a57b34e80565b36e577aa3fabb2b02843cb95b2a
Camilo Vanegas <Camilo.Vanegas@vml.com>
2019-12-05 15:27:54 -0500

    Release stable version

--- spring/js/main.js
+++ spring/js/main.js
@@ -13,6 +13,26 @@ var initSlider = function() {
 		slider.mount();
 	});
 }
+
+// Wow Support 
+var wowInit = function() {
+	var wow = new WOW(
+		{
+			boxClass:     'wow',      // animated element css class (default is wow)
+			animateClass: 'animated', // animation css class (default is animated)
+			offset:       0,          // distance to the element when triggering the animation (default is 0)
+			mobile:       true,       // trigger animations on mobile devices (default is true)
+			live:         true,       // act on asynchronously loaded content (default is true)
+			callback:     function(box) {
+			// the callback is fired every time an animation is started
+			// the argument that is passed in is the DOM node being animated
+			},
+			scrollContainer: null,    // optional scroll container selector, otherwise use window,
+			resetAnimation: true,     // reset animation on end (default is true)
+		}
+	);
+	wow.init();
+}
 // Create map
 var buildMap = function () {
 
@@ -38,13 +58,16 @@ var modalEvents = function() {
 	var modalWrapper = document.querySelector('.modal');
 	// Bubbling click
 	modalWrapper.addEventListener('click', function (e) {
-		if (e.target.classList.contains('modal--close')) {
+		e.preventDefault();
+		if (e.target.classList.contains('modal--close') || e.target.classList.contains('button-project')) {
 			modalWrapper.classList.remove('enable');
 		}
 	})
 }
 
 function init() {
+	// Wow
+	wowInit();
 	// Modals
 	modalEvents();
 	// Trigger slider
